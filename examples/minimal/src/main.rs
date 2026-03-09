a023d1c790c9ff255a47c77438f5d77889ac1758
Drew Chase (Home) <dcmanproductions@gmail.com>
2026-03-08 22:13:18 -0400

    Remove unused `StringColor` references from minimal example

--- examples/minimal/src/main.rs
+++ examples/minimal/src/main.rs
@@ -1,8 +1,3 @@
 fn main() {
     println!("Hello, world!");
-
-    use aurora_ui::color::StringColor;
-
-    let color = "ff00ff".color();       // &str
-    let color = "00ff00".to_string().color();  // String
 }
