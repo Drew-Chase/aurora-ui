6c5a73e15653879de7659c379e99020cf07a5b30
Drew Chase (Home) <dcmanproductions@gmail.com>
2026-03-09 18:56:57 -0400

    Replace "Hello, world!" with a minimal example using `aurora_ui::App`.

--- examples/minimal/src/main.rs
+++ examples/minimal/src/main.rs
@@ -1,3 +1,9 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use aurora_ui::app::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .title("Minimal Example")
+        .run(|_app, _frame_info| {})
+        .expect("Failed to run app");
 }
