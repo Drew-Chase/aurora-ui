f9a2ffafa0ec59ffcffe55fafdf6e02f068b7b59
Drew Chase (Home) <dcmanproductions@gmail.com>
2026-03-11 17:05:12 -0400

    Remove unused code and simplify imports in `aurora_widgets` and `minimal` example

--- examples/minimal/src/main.rs
+++ examples/minimal/src/main.rs
@@ -1,10 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use aurora_ui::app::App;
-use aurora_ui::color::Color;
-use aurora_ui::geometry::corners::Corners;
-use aurora_ui::geometry::point::Point;
-use aurora_ui::geometry::rect::Rect;
-use aurora_ui::geometry::size::Size;
 
 fn main() {
     App::new()
