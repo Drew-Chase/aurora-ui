14426076a03b2c9d265b3182eeb6fb8408ea99e6
Drew Chase (Home) <dcmanproductions@gmail.com>
2026-03-14 14:31:45 -0400

    feat(examples): add `counter_example` showcasing a simple interactive counter widget
    - Introduced a new example `counter_example` demonstrating a counter with increment and decrement buttons.
    - Utilized `aurora_ui` components, including `App`, `button`, and `Text` widgets, for layout and interaction.

---
+++ examples/counter_example/src/main.rs
@@ -0,0 +1,71 @@
+use aurora_ui::prelude::*;
+
+
+fn main() {
+    App::new()
+        .title("Button Example")
+        .size((300, 300))
+        .resizable(false)
+        .font(include_bytes!("../../Roboto-Regular.ttf"))
+        .run(|window, _frame_info| {
+            window.root(
+                column!()
+                    .spacing(10.0)
+                    .align(Align::Center)
+                    .justify(Justify::Center)
+                    .child(counter()),
+            );
+        })
+        .expect("Failed to run app");
+}
+
+#[derive(Default)]
+struct CounterState {
+    value: i32,
+}
+
+pub fn counter() -> impl Widget {
+    Composite::new(CounterState::default(), move |state, set_state| {
+        let decrement_setter = set_state.clone();
+        let increment_setter = set_state.clone();
+
+        Box::new(
+            row!()
+                .height(80)
+                .width(150)
+                .spacing(10.0)
+                .justify(Justify::Center)
+                .align(Align::Center)
+                .child(button(ButtonOptions {
+                    text_options: Text {
+                        text: "-".into(),
+                        align: Align::Center,
+                        ..Text::default()
+                    },
+                    background_color: hex!(0xff0000),
+                    width: 40,
+                    height: 40,
+                    on_click: Box::new(move |_| decrement_setter.set(|prev| prev.value -= 1)),
+                    ..ButtonOptions::default()
+                }))
+                .child(
+                    Text::new(state.value.to_string())
+                        .font_size(20.0)
+                        .align(Align::Center)
+                        .padding(Edges::new(30.0, 0.0, 0.0, 0.0)),
+                )
+                .child(button(ButtonOptions {
+                    text_options: Text {
+                        text: "+".into(),
+                        align: Align::Center,
+                        font_size: 20.0,
+                        ..Text::default()
+                    },
+                    width: 40,
+                    height: 40,
+                    on_click: Box::new(move |_| increment_setter.set(|prev| prev.value += 1)),
+                    ..ButtonOptions::default()
+                })),
+        )
+    })
+}
