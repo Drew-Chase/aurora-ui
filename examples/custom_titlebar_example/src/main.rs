6084db712723e99ad0cab08e71e12a61ee676a38
Drew Chase (Home) <dcmanproductions@gmail.com>
2026-03-15 02:07:25 -0400

    refactor(custom_titlebar_example): simplify touch area handling for draggable window

--- examples/custom_titlebar_example/src/main.rs
+++ examples/custom_titlebar_example/src/main.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use aurora_ui::prelude::*;
 
 fn main() {
@@ -9,16 +10,11 @@ fn main() {
         .custom_titlebar(true)
         .run(|window, _frame_info| {
             let handle = window.window_handle().clone();
-            window.root(
-                col!().child(
-                    TouchArea::new()
-                        .on_mouse_down(move |button| {
-                            if button == MouseButton::Left {
-                                let _ = handle.drag_window();
-                            }
-                        }),
-                ),
-            )
+            window.root(col!().child(TouchArea::new().on_mouse_down(move |button| {
+                if button == MouseButton::Left {
+                    handle.drag_window().expect("Failed to drag window");
+                }
+            })))
         })
         .expect("Failed to run app");
 }
