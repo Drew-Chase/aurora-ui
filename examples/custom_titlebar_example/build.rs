dc670aaaddf4b47b3c34c4cef3988518b5ad9920
Drew Chase (Home) <dcmanproductions@gmail.com>
2026-03-24 21:45:49 -0400

    Fix build.rs: use CARGO_MANIFEST_DIR and resize icon to 256px for ICO

--- examples/custom_titlebar_example/build.rs
+++ examples/custom_titlebar_example/build.rs
@@ -1,19 +1,20 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
     #[cfg(target_os = "windows")]
     {
-        let png_path = &Path::new("../../../logo.png").canonicalize().unwrap();
+        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let png_path = manifest_dir.join("../../logo.png");
         if png_path.exists() {
-            let img = image::open(png_path).expect("Failed to open logo.png");
-            let ico_path =
-                std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("icon.ico");
+            let img = image::open(&png_path).expect("Failed to open logo.png");
+            let img = img.resize(256, 256, image::imageops::FilterType::Lanczos3);
+            let ico_path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("icon.ico");
             img.save(&ico_path).expect("Failed to save icon as ICO");
 
             let mut res = winresource::WindowsResource::new();
             res.set_icon(ico_path.to_str().unwrap());
             res.compile().expect("Failed to compile Windows resources");
-        }else{
+        } else {
             panic!("Failed to find icon: {}", png_path.display());
         }
     }
--- examples/image_example/build.rs
+++ examples/image_example/build.rs
@@ -1,13 +1,14 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
     #[cfg(target_os = "windows")]
     {
-        let png_path = &Path::new("../../../logo.png").canonicalize().unwrap();
+        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let png_path = manifest_dir.join("../../logo.png");
         if png_path.exists() {
-            let img = image::open(png_path).expect("Failed to open logo.png");
-            let ico_path =
-                std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("icon.ico");
+            let img = image::open(&png_path).expect("Failed to open logo.png");
+            let img = img.resize(256, 256, image::imageops::FilterType::Lanczos3);
+            let ico_path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("icon.ico");
             img.save(&ico_path).expect("Failed to save icon as ICO");
 
             let mut res = winresource::WindowsResource::new();
